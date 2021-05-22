3c5f163019adc1d251539ea3ba7045c7c1ffb1b6
Lorenzo Manacorda <lorenzo@mailbox.org>
2021-05-22 15:42:12 +0200

    flake: restructure

--- flake.nix
+++ flake.nix
@@ -9,38 +9,35 @@
     };
   };
 
-  outputs = { self, nixpkgs, polkadot }: {
-
-    overlay = final: prev:
-      let
-        pkgs = nixpkgs.legacyPackages.${final.system};
-        version =
-          let
-            v = pkgs.lib.strings.fileContents ./latest-version;
-          in
-          with builtins; substring 1 (stringLength v) v;
-        hash = pkgs.lib.strings.fileContents ./hash;
-        name = "polkadot-latest";
-        src = polkadot;
-      in
-      {
-        polkadot-latest = pkgs.polkadot.overrideAttrs (old: {
-          inherit src name version;
-
-          cargoDeps = old.cargoDeps.overrideAttrs (_: {
-            inherit src;
-
-            name = "${name}-vendor.tar.gz";
-            outputHash = hash;
-          });
-        });
+  outputs = { self, nixpkgs, polkadot }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      version =
+        let
+          v = pkgs.lib.strings.fileContents ./latest-version;
+        in
+        with builtins; substring 1 (stringLength v) v;
+      hash = pkgs.lib.strings.fileContents ./hash;
+      name = "polkadot-latest";
+      src = polkadot;
+    in
+    {
+      overlay = final: prev: {
+        "${name}" = self.packages.${system}.${name};
       };
 
-    defaultPackage.x86_64-linux =
-      let
-        pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlay ]; };
-      in
-      pkgs.polkadot-latest;
+      packages.${system}.${name} = pkgs.polkadot.overrideAttrs (old: {
+        inherit src name version;
 
-  };
+        cargoDeps = old.cargoDeps.overrideAttrs (_: {
+          inherit src;
+
+          name = "${name}-vendor.tar.gz";
+          outputHash = hash;
+        });
+      });
+
+      defaultPackage.${system} = self.packages.${system}.${name};
+    };
 }
